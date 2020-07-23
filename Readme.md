b0a4fe4fafffe83c83ad776ae116d1eb2c91cf92
MaulingMonkey <git@maulingmonkey.com>
2020-07-23 02:37:11 -0700

    Switch to egli, fix hardcoded library path, don't clear package task

--- Readme.md
+++ Readme.md
@@ -39,6 +39,11 @@ Unless you explicitly state otherwise, any contribution intentionally submitted
 for inclusion in the work by you, as defined in the Apache-2.0 license, shall be
 dual licensed as above, without any additional terms or conditions.
 
+# Takeaways
+
+* `egl` is unsound as heck and unmaintained
+* `khronos-egl` 's pkg_config isn't cross compilation friendly
+* `egli` for the win?
 
 
 [rustup]:       https://rustup.rs/
--- rust-opendingux-test/src/main.rs
+++ rust-opendingux-test/src/main.rs
@@ -7,16 +7,7 @@ fn render() {
 
 #[cfg(target_vendor = "gcw0")]
 fn main() {
-    #[link(name = "EGL")]
-    #[link(name = "GLESv2")]
-    extern {}
-
-    const ATTRIBUTE_LIST : &[egl::EGLint] = &[
-        egl::EGL_RED_SIZE, 1,
-        egl::EGL_GREEN_SIZE, 1,
-        egl::EGL_BLUE_SIZE, 1,
-        egl::EGL_NONE,
-    ];
+    use std::ptr::null_mut;
 
     std::panic::set_hook(Box::new(|info|{
         if let Some(loc) = info.location() {
@@ -37,45 +28,39 @@ fn main() {
         std::thread::sleep(std::time::Duration::from_secs(10));
     }));
 
+    println!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+
     // https://www.khronos.org/registry/EGL/sdk/docs/man/html/eglIntro.xhtml
-    let display = egl::get_display(egl::EGL_DEFAULT_DISPLAY).expect("No default display");
-    // UNSOUND?  `display_id` is a raw pointer.
+    // https://docs.rs/crate/egli/0.5.0
+    let display = egli::Display::from_default_display().expect("No default display");
+    display.initialize().expect("EGL failed to initialize");
+    let config = *display
+        .config_filter()
+        .with_red_size(1)
+        .with_green_size(1)
+        .with_blue_size(1)
+        //.with_conformant(egli::RenderableType::OPENGL_ES2)
+        .choose_configs()
+        .expect("Error querying suitable EGL configurations")
+        .first()
+        .expect("No suitable EGL configurations");
 
-    let (mut major, mut minor) = (0, 0);
-    assert!(egl::initialize(display, &mut major, &mut minor), "EGL failed to initialize");
-    // UNSOUND?  `display` is a raw pointer.
-
-    let config = egl::choose_config(display, ATTRIBUTE_LIST, 1).expect("No matching EGL configuration");
-    // UNSOUND AS HECK API WITH TOTALLY WRONG TYPE SIGNATURE?
-    // 1. display is a raw pointer.
-    // 2. no guarantee that ATTRIBUTE_LIST is terminated by EGL_NONE, which the underlying C fn requires
-    // 3. config_size should correspond to the size of a configs array.  Instead, internally this has a single config always for trivial buffer overflows:
-    // ```no_run
-    // let mut config: EGLConfig = ptr::null_mut();
-    // ```
-    // Despite these issues, this function isn't marked unsafe!
-
-    let context = egl::create_context(display, config, egl::EGL_NO_CONTEXT, &[]).expect("Unable to create EGL context");
-    // UNSOUND (pointers)
-
-    let surface = egl::create_window_surface(display, config, std::ptr::null_mut(), &[]).expect("Unable to create EGL surface");
-    // UNSOUND (pointers)
-
-    assert!(egl::make_current(display, surface, surface, context), "Unable to make EGL context current");
-    // UNSOUND (pointers)
+    let context = display.create_context(config).expect("Unable to create EGL context");
+    let surface = display.create_window_surface(config, null_mut()).expect("Unable to create EGL surface");
+    display.make_current(&surface, &surface, &context).expect("Unable to make EGL context current");
 
     // Give us a second to see logging
     println!("OpenGL initialized, will start rendering in 3 seconds");
     std::thread::sleep(std::time::Duration::from_secs(3));
 
-    gl::ClearColor::load_with(|proc| egl::get_proc_address(proc) as _);
-    gl::Clear     ::load_with(|proc| egl::get_proc_address(proc) as _);
-    gl::Flush     ::load_with(|proc| egl::get_proc_address(proc) as _);
+    gl::ClearColor::load_with(|proc| egli::egl::get_proc_address(proc) as _);
+    gl::Clear     ::load_with(|proc| egli::egl::get_proc_address(proc) as _);
+    gl::Flush     ::load_with(|proc| egli::egl::get_proc_address(proc) as _);
 
     loop {
         render();
         unsafe { gl::Flush() };
-        egl::swap_buffers(display, surface);
+        display.swap_buffers(&surface).expect("failed to swap buffers");
     }
 }
 
@@ -83,6 +68,8 @@ fn main() {
 
 #[cfg(not(target_vendor = "gcw0"))]
 fn main() {
+    println!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+
     use glutin::event::{Event, KeyboardInput, VirtualKeyCode, WindowEvent};
     use glutin::event_loop::{ControlFlow, EventLoop};
     use glutin::window::{Fullscreen, WindowBuilder};
